2653cf49af0f3075787a38c76d658d14a4e61e38
Hiroaki Akanuma <hiroaki.akanuma@gmail.com>
2018-10-26 09:13:57 +0900

    Blink LED

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.OUT)
 
 while True:
-    print time.ctime()
-    time.sleep(10)
+    GPIO.output(2,True)
+    time.sleep(2)
+    GPIO.output(2,False)
+    time.sleep(2)
